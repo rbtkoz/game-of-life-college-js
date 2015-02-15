521efd6c6ca282c593a9ce8e025d623e1bebd507
AK <kozovski.a@gmail.com>
2015-02-15 18:03:52 -0500

    working

--- gameoflife.js
+++ gameoflife.js
@@ -17,18 +17,20 @@ var gameOfLife = {
     var goltable = document.createElement("tbody");
     goltable.innerHTML = tablehtml;
     $('#board').append(goltable);
-    
+
     // once html elements are added to the page, attach events to them
     this.setupBoardEvents();
   },
 
   forEachCell: function (func) {
-    /* 
-      COLLEGE.JS: Write forEachCell here. You will have to visit
-                  each cell on the board, call the "func" function,
-                  and pass into func, the cell and the cell's x & y
-                  coordinates. for example: func(cell, x, y)
-    */
+
+    for(y=0;y<this.height;y++){
+      for(x=0;x<this.width;x++){
+        var target = "#"+x+"-"+y;
+        func($(target),x,y );
+      }
+    }
+
   },
 
   toggleCell: function(cell) {
@@ -73,21 +75,73 @@ var gameOfLife = {
   countAliveNeighbors: function() {
     this.forEachCell(function (cell, x, y) {
         var aliveNeighbors = 0;
+        //top is y -1
+        //top left is x-1 y-1
+        //top right  x+1 y -1
+        // left x-1
+        // right  x+1
+        //bottom y+1
+        //bottom left x-1 y+1
+        //bottom right x+1 y+1
+        var status = $(cell).attr('data-status');
+          for (a =-1;a<2;a++){
+            for(b = -1;b<2;b++){
+              var x_nbr = a+x;
+              var y_nbr = b+y;
+              // console.log(x,y,"dead",":",x_nbr, y_nbr,"neighbor");
+              var neighbor_id = "#"+x_nbr+"-"+y_nbr;
+              var neighbor_st = $(neighbor_id).attr('data-status');
+                // debugger;
+
+                if(!(x_nbr==x && y_nbr==y) && neighbor_st ==="alive"){
+                  aliveNeighbors++;
+
+
+                }
+            }
+          }
+
 
         /*
             COLLEGE.JS: Given the cell object, find the neighbors and figure out
                         how many alive neighbors there are.
         */
-
+        // debugger;
         $(cell).attr('data-neighbors', aliveNeighbors);
     });
   },
-  
+
   determineAndSetNextState: function (argument) {
     var getNextState = function (currentState, numAliveNeighbors) {
-      /* 
-        COLLEGE.JS: Apply rules of Game of Life and return "dead" or "alive"
-      */
+      if (currentState === "alive"){
+        if(numAliveNeighbors === 2 || numAliveNeighbors ===3){
+          // console.log(currentState,numAliveNeighbors,":","alive");
+          return currentState;
+        }else if(numAliveNeighbors < 2){
+          // console.log(currentState,numAliveNeighbors,":","dead");
+          return "dead" ;
+        }else if(numAliveNeighbors > 3){
+          // console.log(currentState,numAliveNeighbors,":","dead");
+          return "dead" ;
+        }else{
+          return currentState;
+        }
+
+      }else{
+        if(numAliveNeighbors === 3){
+          // console.log(currentState,numAliveNeighbors,":","alive");
+          return "alive" ;
+        }else{
+          return currentState;
+        }
+
+      }
+      // Any live cell with two or three live neighbors lives on to the next generation.
+      // Any live cell with fewer than two live neighbors dies, as if caused by under-population.
+      // Any live cell with more than three live neighbors dies, as if by overcrowdi
+      //Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction
+
+
     };
 
     this.forEachCell(function (cell, x, y) {
@@ -116,8 +170,8 @@ var gameOfLife = {
   },
 
   enableAutoPlay: function () {
-    /* 
-      COLLEGE.JS: Write code here to call the step function repeatedly every 200ms 
+    /*
+      COLLEGE.JS: Write code here to call the step function repeatedly every 200ms
                   Look up the "setInterval" and "clearInterval" functions to learn
                   how to do this. You can also use the above "togglePlayPause" function
                   to toggle the button between "Play" and "Pause".
@@ -127,19 +181,3 @@ var gameOfLife = {
 };
 
 gameOfLife.createAndShowBoard();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
